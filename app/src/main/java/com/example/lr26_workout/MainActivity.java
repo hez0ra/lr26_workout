8c800436d877a38c8f6e6e9b02e9a9a3718d1935
hez0ra <feleepen@gmail.com>
2024-10-11 11:26:31 +0300

    первый коммит

---
+++ app/src/main/java/com/example/lr26_workout/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lr26_workout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.lr26_workout.fragments.WorkoutListFragment;
+
+public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(long id) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
+        startActivity(intent);
+    }
+}
